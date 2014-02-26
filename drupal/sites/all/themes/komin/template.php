07485feb3114e731eac9874a7d9b5a469b246e2d
Bror & Co <jdg@bror.se>
2014-02-26 08:45:16 +0100

    Added malmostad classes for form elements

--- drupal/sites/all/themes/komin/template.php
+++ drupal/sites/all/themes/komin/template.php
@@ -12,3 +12,218 @@ function komin_preprocess_html(&$variables) {
   // Set development class
   $variables['classes_array'][] = 'development';
 }
+
+
+
+function komin_form_alter(&$form, &$form_state, $form_id) {
+
+  /*
+   * Add class to form tag
+   */
+  $additional_classes = array();
+  foreach ($form['#attributes']['class'] as $class) {
+    switch ($class) {
+      case 'node-forum-form':
+        $additional_classes[] = 'form-horizontal';
+        $additional_classes[] = 'control-group';
+        break;
+    }
+  }
+
+  foreach ($additional_classes as $class) {
+    if (!in_array($class, $form['#attributes']['class'])) {
+      $form['#attributes']['class'][] = $class;
+    }
+  }
+
+}
+
+function komin_form_element(&$variables) {
+  $element = & $variables['element'];
+  $is_checkbox = FALSE;
+  $is_radio = FALSE;
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+
+  // Check for errors and set correct error class.
+  if (isset($element['#parents']) && form_get_error($element)) {
+    $attributes['class'][] = 'error';
+  }
+
+  if (!empty($element['#type'])) {
+    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+  }
+  if (!empty($element['#name'])) {
+    $attributes['class'][] = 'form-item-' . strtr(
+        $element['#name'], array(
+          ' ' => '-',
+          '_' => '-',
+          '[' => '-',
+          ']' => '',
+        )
+      );
+  }
+
+  // Malmostad
+  $attributes['class'][] = 'control-group';
+
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element['#attributes']['disabled'])) {
+    $attributes['class'][] = 'form-disabled';
+  }
+  if (!empty($element['#autocomplete_path']) && drupal_valid_path($element['#autocomplete_path'])) {
+    $attributes['class'][] = 'form-autocomplete';
+  }
+  $attributes['class'][] = 'form-item';
+
+  // See http://getbootstrap.com/css/#forms-controls.
+  if (isset($element['#type'])) {
+    if ($element['#type'] == "radio") {
+      $attributes['class'][] = 'radio';
+      $is_radio = TRUE;
+    }
+    elseif ($element['#type'] == "checkbox") {
+      $attributes['class'][] = 'checkbox';
+      $is_checkbox = TRUE;
+    }
+    else {
+      $attributes['class'][] = 'form-group';
+    }
+  }
+
+  $description = FALSE;
+  $tooltip = FALSE;
+  // Convert some descriptions to tooltips.
+  // @see bootstrap_tooltip_descriptions setting in _bootstrap_settings_form()
+  if (!empty($element['#description'])) {
+    $description = $element['#description'];
+    if (theme_get_setting('bootstrap_tooltip_enabled') && theme_get_setting('bootstrap_tooltip_descriptions')
+      && $description === strip_tags($description)
+      && strlen($description) <= 200
+    ) {
+      $tooltip = TRUE;
+      $attributes['data-toggle'] = 'tooltip';
+      $attributes['title'] = $description;
+    }
+  }
+
+  $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+
+  $prefix = '<div class="controls">';
+  $suffix = '</div>';
+  if (isset($element['#field_prefix']) || isset($element['#field_suffix'])) {
+    // Determine if "#input_group" was specified.
+    if (!empty($element['#input_group'])) {
+      $prefix .= '<div class="input-group">';
+      $prefix .= isset($element['#field_prefix']) ?
+        '<span class="input-group-addon">' . $element['#field_prefix'] . '</span>' : '';
+      $suffix .= isset($element['#field_suffix']) ?
+        '<span class="input-group-addon">' . $element['#field_suffix'] . '</span>' : '';
+      $suffix .= '</div>';
+    }
+    else {
+      $prefix .= isset($element['#field_prefix']) ? $element['#field_prefix'] : '';
+      $suffix .= isset($element['#field_suffix']) ? $element['#field_suffix'] : '';
+    }
+  }
+
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      if ($is_radio || $is_checkbox) {
+        $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      }
+      else {
+        $variables['#children'] = ' ' . $prefix . $element['#children'] . $suffix;
+      }
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if ($description && !$tooltip) {
+    $output .= '<p class="help-block">' . $element['#description'] . "</p>\n";
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
+}
+
+function komin_form_element_label(&$variables) {
+  $element = $variables['element'];
+
+  // This is also used in the installer, pre-database setup.
+  $t = get_t();
+
+  // Determine if certain things should skip for checkbox or radio elements.
+  $skip = (isset($element['#type']) && ('checkbox' === $element['#type'] || 'radio' === $element['#type']));
+
+  // If title and required marker are both empty, output no label.
+  if ((!isset($element['#title']) || $element['#title'] === '' && !$skip) && empty($element['#required'])) {
+    return '';
+  }
+
+  // If the element is required, a required marker is appended to the label.
+  $required = !empty($element['#required']) ? theme('form_required_marker', array('element' => $element)) : '';
+
+  $title = filter_xss_admin($element['#title']);
+
+  $attributes = array();
+
+  // Style the label as class option to display inline with the element.
+  if ($element['#title_display'] == 'after' && !$skip) {
+    $attributes['class'][] = $element['#type'];
+  }
+  // Show label only to screen readers to avoid disruption in visual flows.
+  elseif ($element['#title_display'] == 'invisible') {
+    $attributes['class'][] = 'element-invisible';
+  }
+
+  // Malmostad
+  $attributes['class'][] = 'control-label';
+
+  if (!empty($element['#id'])) {
+    $attributes['for'] = $element['#id'];
+  }
+
+  // Insert radio and checkboxes inside label elements.
+  $output = '';
+  if (isset($variables['#children'])) {
+    $output .= $variables['#children'];
+  }
+
+  // Append label.
+  $output .= $t('!title !required', array('!title' => $title, '!required' => $required));
+
+  // The leading whitespace helps visually separate fields from inline labels.
+  return ' <label' . drupal_attributes($attributes) . '>' . $output . "</label>\n";
+}
+
+function komin_field_widget_form_alter(&$element, &$form_state, $context) {
+  $element['#attributes']['class'][] = 'input-wide';
+}
---
+++ drupal/sites/all/themes/komin/theme/page--node--add--forum.tpl.php
@@ -0,0 +1,55 @@
+<header>
+  <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+    <div>
+      <nav role="navigation">
+        <?php if (!empty($primary_nav)): ?>
+          <?php print render($primary_nav); ?>
+        <?php endif; ?>
+        <?php if (!empty($secondary_nav)): ?>
+          <?php print render($secondary_nav); ?>
+        <?php endif; ?>
+        <?php if (!empty($page['navigation'])): ?>
+          <?php print render($page['navigation']); ?>
+        <?php endif; ?>
+      </nav>
+    </div>
+  <?php endif; ?>
+</header>
+
+<div class="main-container">
+  <?php if (!empty($page['sidebar_first'])): ?>
+    <aside>
+      <?php print render($page['sidebar_first']); ?>
+    </aside>
+  <?php endif; ?>
+
+  <acticle>
+    <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
+    <?php print render($title_prefix); ?>
+    <?php if (!empty($title)): ?>
+      <h1 class="page-header"><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php print $messages; ?>
+    <?php if (!empty($tabs)): ?>
+      <?php print render($tabs); ?>
+    <?php endif; ?>
+    <?php if (!empty($page['help'])): ?>
+      <?php print render($page['help']); ?>
+    <?php endif; ?>
+    <?php if (!empty($action_links)): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+  </acticle>
+
+  <?php if (!empty($page['sidebar_second'])): ?>
+    <aside>
+      <?php print render($page['sidebar_second']); ?>
+    </aside>
+  <?php endif; ?>
+
+</div>
+<footer>
+  <?php print render($page['footer']); ?>
+</footer>
